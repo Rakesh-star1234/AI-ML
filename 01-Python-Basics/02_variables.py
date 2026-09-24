bd7dd73751b1bc5ea79d92d83369e1f133c2e817
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-24 12:19:29 +0530

    Practice Python input and variables

--- 01-Python-Basics/02_variables.py
+++ 01-Python-Basics/02_variables.py
@@ -9,7 +9,7 @@ print(city)
 print(type(name))
 print(type(age))
 print(type(city))
-name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 
 print("Hello", name) 
 age = input("Enter your age: ")
