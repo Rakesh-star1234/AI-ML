4ccbba627bf63c9f15985747f9bc4515f16832ba
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-24 12:32:27 +0530

    Complete Python data types basics

--- 01-Python-Basics/03_data_types.py
+++ 01-Python-Basics/03_data_types.py
@@ -7,3 +7,27 @@ print(type(name))
 print(type(age))
 print(type(height))
 print(type(is_student))
+course = "AI/ML"
+
+print(course)
+print(type(course))
+marks = 85
+
+print(marks)
+print(type(marks))
+percentage = 84.5
+
+print(percentage)
+print(type(percentage))
+is_learning = True
+
+print(is_learning)
+print(type(is_learning))
+is_learning = False
+
+print(is_learning)
+print(type(is_learning))
+result = None
+
+print(result)
+print(type(result))
