bbdba2055867778114532fafd0f14e616a10f11c
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-24 12:42:37 +0530

    Complete Python operators basics

---
+++ 01-Python-Basics/04_operators.py
@@ -0,0 +1,59 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a // b)
+print(a ** b)
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+print(a > 5 and b < 5)
+print(a > 20 or b < 5)
+print(not(a > b))
+x = 10
+
+x += 5
+print(x)
+
+x -= 3
+print(x)
+
+x *= 2
+print(x)
+
+x /= 4
+print(x)
+x = 10
+
+x //= 3
+print(x)
+
+x %= 2
+print(x)
+
+x **= 3
+print(x)
+name = "Rakesh"
+
+print("R" in name)
+print("z" in name)
+print("z" not in name)
+a = 10
+b = 10
+
+print(a is b)
+print(a is not b)
+a = 5
+b = 3
+
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
