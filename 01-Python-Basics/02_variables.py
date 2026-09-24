a6d239ceeb824d84bf4f6b0282cdb3abab400bef
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-24 12:25:34 +0530

    Complete Python variables basics

--- 01-Python-Basics/02_variables.py
+++ 01-Python-Basics/02_variables.py
@@ -23,3 +23,15 @@ is_student = True
 
 print(height)
 print(is_student)
+print("My name is", name)
+print("My age is", age)
+print("My height is", height)
+print("Am I a student?", is_student)
+age = 30
+
+print("Updated age:", age)
+x, y, z = 10, 20, 30
+
+print(x)
+print(y)
+print(z)
