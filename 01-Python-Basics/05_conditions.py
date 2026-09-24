dd7320d95d2485522bb6575da23423d722f7d0e8
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-24 13:01:22 +0530

    Practice Python conditions

--- 01-Python-Basics/05_conditions.py
+++ 01-Python-Basics/05_conditions.py
@@ -1,98 +1,23 @@
-age = 20
-
-if age >= 18:
-    print("You are eligible")
-else:
-    print("You are not eligible")
-
-
-
-marks = 75
-
-if marks >= 90:
-    print("Grade A")
-elif marks >= 60:
-    print("Grade B")
-else:
-    print("Grade C") 
-    age = 16
-
-
-
-
-if age >= 18:
-    print("You are eligible")
-else:
-    print("You are not eligible")
-
-
-
-
-    marks = 75
-
-if marks >= 90:
-    print("Grade A")
-elif marks >= 60:
-    print("Grade B")
-else:
-    print("Grade C")
-
-
-
-
-
-
-    age = 20
-has_id = True
-
-if age >= 18:
-    if has_id:
-        print("Entry allowed")
-    else:
-        print("ID required")
-else:
-    print("Age is below 18")
-
-
-
-
-    age = 22
-marks = 70
-
-if age >= 18 and marks >= 60:
-    print("Eligible")
-else:
-    print("Not eligible")
-
-
-
-    age = 16
-has_permission = True
-
-if age >= 18 or has_permission:
-    print("Entry allowed")
-else:
-    print("Entry not allowed")
-
-
-
-
-    is_raining = False
-
-if not is_raining:
-    print("You can go outside")
-else:
-    print("Take an umbrella")
-
-
-
-
-    number = 10
+number = 10
 
 if number % 2 == 0:
     print("Even number")
 else:
     print("Odd number")
+
+
+
+
+    number = -5
+
+if number > 0:
+    print("Positive number")
+elif number < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+   
     
 
     
