c53d9f2ef17be33f51bd08f725cc450862ac4316
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-25 17:58:26 +0530

    Practice Python while loop

--- 01-Python-Basics/06_loops.py
+++ 01-Python-Basics/06_loops.py
@@ -16,3 +16,15 @@ for i in range(1, 11):
 for i in range(1, 11):
     if i % 2 != 0:
         print(i) 
+        number = 5
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i = i + 1
+    i = 2
+
+while i <= 10:
+    print(i)
+    i = i + 2
